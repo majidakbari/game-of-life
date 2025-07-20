4666cd008a8dccf3d4bcfb2a88ac2bf6c31b5bca
Majid Akbari <majidakbariiii@gmail.com>
2025-07-20 13:53:52 +0300

    setup phpunit

---
+++ tests/Entities/CellTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Entities\Cell;
+use PHPUnit\Framework\TestCase;
+
+class CellTest extends TestCase
+{
+    public function testCoordinatesShouldWork(): void
+    {
+        // arrange
+        $sut = new Cell(100, 20);
+
+        // act
+        $actual = $sut->coordinates();
+
+        // assert
+        $this->assertSame('100,20', $actual);
+    }
+}
