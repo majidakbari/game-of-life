6831af431e979a2076a4d95a9bf424e0e93f6def
Majid Akbari <majidakbariiii@gmail.com>
2025-07-20 19:44:49 +0300

    refactor GameOfLife further and extract a few methods to make the code clean and readable

--- app/Domain/GameOfLife.php
+++ app/Domain/GameOfLife.php
@@ -9,11 +9,21 @@ class GameOfLife implements GameInterface
 {
     public function advance(Grid $grid): Grid
     {
-        $neighborCounts = [];
+        $liveNeighbours = $this->countLiveNeighbours($grid);
+        $newLiveCells = $this->applyRules($liveNeighbours, $grid);
+        return new Grid($grid->getSize(), $newLiveCells);
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    public function countLiveNeighbours(Grid $grid): array
+    {
+        $output = [];
         foreach ($grid->getLiveCells() as $key => $_) {
             [$x, $y] = explode(',', $key);
-            $x = (int) $x;
-            $y = (int) $y;
+            $x = (int)$x;
+            $y = (int)$y;
             for ($dx = -1; $dx <= 1; $dx++) {
                 for ($dy = -1; $dy <= 1; $dy++) {
                     $nx = $x + $dx;
@@ -22,13 +32,20 @@ class GameOfLife implements GameInterface
                         continue;
                     }
                     $nKey = "$nx,$ny";
-                    $neighborCounts[$nKey] = ($neighborCounts[$nKey] ?? 0) + 1;
+                    $output[$nKey] = ($output[$nKey] ?? 0) + 1;
                 }
             }
         }
+        return $output;
+    }
 
+    /**
+     * @return Cell[]
+     */
+    public function applyRules(array $liveNeighbours, Grid $grid): array
+    {
         $newLiveCells = [];
-        foreach ($neighborCounts as $key => $count) {
+        foreach ($liveNeighbours as $key => $count) {
             [$cellX, $cellY] = explode(',', $key);
             $targetCell = new Cell($cellX, $cellY);
             $alive = $grid->isAlive($targetCell);
@@ -36,6 +53,6 @@ class GameOfLife implements GameInterface
                 $newLiveCells[] = $targetCell;
             }
         }
-        return new Grid($grid->getSize(), $newLiveCells);
+        return $newLiveCells;
     }
 }
